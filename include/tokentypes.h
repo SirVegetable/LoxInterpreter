6b35dedd76913e39105e23ab445e5804e901be93
SirVegetable <17wgs@queensu.ca>
2024-06-23 00:38:53 -0400

    all tokens for parsing

---
+++ include/tokentypes.h
@@ -0,0 +1,22 @@
+
+
+enum class TokenTypes
+{
+    // Single-character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, 
+    COMMA, DOT, PLUS, MINUS, SEMICOLON, SLASH, STAR, 
+
+    // One or Two character tokens S
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, 
+    GREATER, GREATER_EQUAL, 
+    LESS, LESS_EQUAL,
+
+    // literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // keywords
+    AND, OR, CLASS, IF, ELSE, FUN, FOR, NIL,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+    
+    EOF
+}; 
