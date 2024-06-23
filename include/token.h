867cb92b326dbc8c90c5e79219e06a0bf09d599d
SirVegetable <17wgs@queensu.ca>
2024-06-23 17:17:07 -0400

    token class for tokenization

--- include/token.h
+++ include/token.h
@@ -9,12 +9,34 @@ class Token
     public: 
         Token(TokenTypes t, std::string l, std::any li, int i); 
         
-        std::string convert_to_string(); 
+        std::string convert_to_string();
     private: 
         TokenTypes type;
         std::string lexeme;
         std::any literal; 
-        int line; 
+        int line;
+
+    private:
+
+        template<typename T>
+        std::string literal_to_string(const T& value)
+        {
+            if(value == TokenTypes::NUMBER)
+            {
+                return std::to_string(static_cast<int>(value)); 
+            }
+            else if(value == TokenTypes::STRING)
+            {
+                return std::to_string(value); 
+            }
+            else 
+            {
+                return ""; 
+            }
+
+            
+        }
+
 
 }; 
 
--- src/token.cpp
+++ src/token.cpp
@@ -1,9 +1,12 @@
 #include "../include/token.h"
+#include <string>
 
 Token::Token(TokenTypes t, std::string l, std::any li, int i) : type{t} , lexeme{l}, literal{li}, line{i}
 {}
 
 std::string Token::convert_to_string()
 {
-    
+    auto first = std::to_string(static_cast<int>(type));
+   
+    return first + " " + lexeme + " " + literal_to_string(literal);   
 }
